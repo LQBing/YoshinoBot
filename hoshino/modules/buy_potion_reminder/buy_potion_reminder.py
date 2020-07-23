347dbf87762cf7c3797cff5cd628873a94ec81b0
Suctance <suctance@parclex.io>
2020-07-24 03:19:27 +0800

    买药水提醒默认关闭

--- hoshino/modules/buy_potion_reminder/buy_potion_reminder.py
+++ hoshino/modules/buy_potion_reminder/buy_potion_reminder.py
@@ -1,8 +1,6 @@
-import pytz
-from datetime import datetime
 from hoshino import Service, R
 
-sv = Service('buy_potion_reminder', enable_on_default=False, help_='买药提醒')
+sv = Service('buy_potion_reminder', enable_on_default=False, help_='买药提醒', bundle='帮助pcr订阅')
 
 
 @sv.scheduled_job('cron', hour='*/6')
