2be5f4be4190cbfbd5d6418fbfbc5cea625cb637
Suctance <suctance@parclex.io>
2020-11-05 22:18:43 +0800

    多语录

--- hoshino/config/mantra.py
+++ hoshino/config/mantra.py
@@ -1,3 +1,4 @@
 from hoshino.util import get_environ
 
-KEYWORD = get_environ('MANTRA_KEYWORD', r'艹一下')
+# KEYWORD = get_environ('MANTRA_KEYWORD', r'艹一下')
+MANTRA_MATRIX = {"艹一下": {"path": "cao", "name": "小艹"}, "YGNB": {"path": "YG", "name": "羊鸽"}}
--- hoshino/modules/mantra/mantra.py
+++ hoshino/modules/mantra/mantra.py
@@ -3,9 +3,9 @@ import random
 
 from nonebot.exceptions import CQHttpError
 
-from hoshino import R, Service, priv
+from hoshino import R, Service, priv, util
 from hoshino.util import FreqLimiter, DailyNumberLimiter
-from hoshino.config.mantra import KEYWORD
+from hoshino.config.mantra import MANTRA_MATRIX
 
 _max = 50
 _fl = 60
@@ -17,24 +17,28 @@ sv = Service('mantra', manage_priv=priv.SUPERUSER, enable_on_default=False, visi
 mantra_folder = R.img('mantra/').path
 
 
-def mantra_generator():
+def mantra_generator(name):
     while True:
         file_list = os.listdir(mantra_folder)
         random.shuffle(file_list)
         for filename in file_list:
-            if os.path.isfile(os.path.join(mantra_folder, filename)):
-                yield R.img('mantra/', filename.replace('\\', '/'))
+            if os.path.isfile(os.path.join(mantra_folder, MANTRA_MATRIX[name]['path'], filename.replace('\\', '/'))):
+                yield R.img(os.path.join('mantra/', MANTRA_MATRIX[name]['path'], filename.replace('\\', '/')))
 
 
-mantra_generator = mantra_generator()
+mantra_generators = dict()
+
+for i in MANTRA_MATRIX:
+    mantra_generators[i] = mantra_generator(i)
 
 
-def get_mantra():
-    return mantra_generator.__next__()
+def get_mantra(name):
+    return mantra_generators[name].__next__()
 
 
-@sv.on_rex(KEYWORD)
+@sv.on_fullmatch(MANTRA_MATRIX)
 async def mantra(bot, ev):
+    name = util.normalize_str(ev.message.extract_plain_text())
     """随机叫一份真言语录，对每个用户有冷却时间"""
     uid = ev['user_id']
     if not _n_limit.check(uid):
@@ -42,14 +46,15 @@ async def mantra(bot, ev):
         return
     if not _f_limit.check(uid):
         limited = True
-        await bot.send(ev, f'您迫害的太频繁了，距离上次迫害还不到{_fl}秒，爱护小艹人人有责。迫害虽棒可不要贪哦(๑•̀ㅂ•́)و✧', at_sender=True)
+        await bot.send(ev, f'您迫害的太频繁了，距离上次迫害还不到{_fl}秒，爱护{MANTRA_MATRIX[name]["name"]}人人有责。迫害虽棒可不要贪哦(๑•̀ㅂ•́)و✧',
+                       at_sender=True)
         if limited:
             return
     _f_limit.start_cd(uid)
     _n_limit.increase(uid)
 
     # conditions all ok, send a mantra.
-    pic = get_mantra()
+    pic = get_mantra(name)
     try:
         await bot.send(ev, pic.cqcode)
     except CQHttpError:
