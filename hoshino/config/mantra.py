643a86227d3ae43f9210f343240cd30131fd178c
Suctance <suctance@parclex.io>
2020-11-05 23:21:31 +0800

    多语录

--- hoshino/config/mantra.py
+++ hoshino/config/mantra.py
@@ -1,4 +1,5 @@
 from hoshino.util import get_environ
 
 # KEYWORD = get_environ('MANTRA_KEYWORD', r'艹一下')
-MANTRA_MATRIX = {"艹一下": {"path": "cao", "name": "小艹"}, "YGNB": {"path": "YG", "name": "羊鸽"}}
+MANTRA_MATRIX = {"艹一下": {"path": "cao", "name": "小艹"}, "YGNB": {"path": "YG", "name": "羊鸽"},
+                 "ygnb": {"path": "YG", "name": "羊鸽"}}
