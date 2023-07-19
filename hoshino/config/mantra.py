a3f79ee56a3e43804425f0b07aff0e1d2477b226
LQBing <lqbing@msn.com>
2023-07-19 11:09:53 +0800

    optimize mantra

--- hoshino/config/mantra.py
+++ hoshino/config/mantra.py
@@ -1,6 +1,4 @@
-import json
 from hoshino.util import get_environ
-from hoshino import R
 
 MANTRA_CONFIG_PATH = get_environ('MANTRA_CONFIG_PATH', 'mantra.json')
 # [
@@ -19,42 +17,3 @@ MANTRA_CONFIG_PATH = get_environ('MANTRA_CONFIG_PATH', 'mantra.json')
 #     }
 # ]
 
-
-class MantraConfig():
-    config_list = []
-    keywords = []
-
-    def __init__(self, config_file_path) -> None:
-        data = None
-        with open(config_file_path, 'r', encoding='utf-8') as fp:
-            data = json.load(fp)
-        print("mantra config : %s" % data)
-        self.config_list = data
-        self.keywords = []
-        for item in self.config_list:
-            for k in item['keywords']:
-                self.keywords.append(k)
-
-    def get_keywords(self):
-        return self.keywords
-
-    def get_path_list(self, keyword):
-        path_list = []
-        for config in self.config_list:
-            if keyword in config['keywords']:
-                path_list = config['path_list']
-        return path_list
-
-    def get_config(self, keyword):
-        for config in self.config_list:
-            if keyword in config['keywords']:
-                return config
-        return {}
-
-    def allow_group(self, keyword, group_id):
-        config = self.get_config(keyword=keyword)
-        if config:
-            if 'group_list' in config:
-                if group_id not in config['group_list']:
-                    return False
-        return True
--- hoshino/modules/mantra/mantra.py
+++ hoshino/modules/mantra/mantra.py
@@ -1,11 +1,54 @@
 import os
+import json
 import random
 
 from nonebot.exceptions import CQHttpError
 
-from hoshino import R, Service, priv, util
+from hoshino import R, Service, priv
 from hoshino.util import FreqLimiter, DailyNumberLimiter
-from hoshino.config.mantra import MANTRA_CONFIG_PATH, MantraConfig
+from hoshino.config.mantra import MANTRA_CONFIG_PATH
+
+
+class MantraConfig():
+    config_list = []
+    keywords = []
+
+    def __init__(self, config_file_path) -> None:
+        data = None
+        with open(config_file_path, 'r', encoding='utf-8') as fp:
+            data = json.load(fp)
+        print("mantra config : %s" % data)
+        self.config_list = data
+        self.keywords = []
+        for item in self.config_list:
+            for k in item['keywords']:
+                self.keywords.append(k)
+
+    def get_keywords(self):
+        return self.keywords
+
+    def get_path_list(self, keyword):
+        path_list = []
+        for config in self.config_list:
+            if keyword in config['keywords']:
+                path_list = config['path_list']
+        return path_list
+
+    def get_config(self, keyword):
+        for config in self.config_list:
+            if keyword in config['keywords']:
+                return config
+        return {}
+
+    def allow_group(self, keyword, group_id):
+        config = self.get_config(keyword=keyword)
+        if config:
+            if 'group_list' in config:
+                if str(group_id) not in config['group_list']:
+                    return False
+        return True
+
+
 MANTRA_CONFIG = MantraConfig(
     R.img(os.path.join('mantra', MANTRA_CONFIG_PATH)).path)
 MANTRA_KEYWORDS = MANTRA_CONFIG.get_keywords()
@@ -59,7 +102,7 @@ async def mantra(bot, ev):
         if not _f_limit.check(uid):
             limited = True
             await bot.send(ev, f'您迫害的太频繁了，距离上次迫害还不到{_fl}秒，爱护{name}人人有责。迫害虽棒可不要贪哦(๑•̀ㅂ•́)و✧',
-                        at_sender=True)
+                           at_sender=True)
             if limited:
                 return
         _f_limit.start_cd(uid)
